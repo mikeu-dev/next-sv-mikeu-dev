787c3ca5dc915ae236d83d91ae3e69871f09536d
Mikeu Dev <rikiruswandi28@gmail.com>
2025-10-24 10:09:36 +0700

    feat:favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import { onMount } from 'svelte';
 	import { Toaster } from 'svelte-sonner';
 	import { ModeWatcher } from 'mode-watcher';
