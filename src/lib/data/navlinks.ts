1708a80f9a719d68a10fd455dd4ced577f6cc26b
Mikeu Dev <rikiruswandi28@gmail.com>
2025-10-27 15:30:51 +0700

    feat:navlink data

---
+++ src/lib/data/navlinks.ts
@@ -0,0 +1,16 @@
+export const navLinks = {
+	en: [
+		{ href: '/', label: 'Home' },
+		{ href: '/projects', label: 'Projects' },
+		{ href: '/blog', label: 'Blog' },
+		{ href: '/about', label: 'About' },
+		{ href: '/contact', label: 'Contact' }
+	],
+	id: [
+		{ href: '/', label: 'Beranda' },
+		{ href: '/projects', label: 'Proyek' },
+		{ href: '/blog', label: 'Blog' },
+		{ href: '/about', label: 'Tentang' },
+		{ href: '/contact', label: 'Kontak' }
+	]
+};
