233031c278d55b1eee67e2897f2aef0c35fd9120
Mikeu Dev <rikiruswandi28@gmail.com>
2025-10-27 15:46:39 +0700

    feat:localization

--- src/lib/components/guest/sections/contact/contact.svelte
+++ src/lib/components/guest/sections/contact/contact.svelte
@@ -3,6 +3,7 @@
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 	import { ArrowRight } from '@lucide/svelte';
+	import * as m from '@/lib/paraglide/messages';
 
 	let contactSection: HTMLElement;
 
@@ -28,17 +29,17 @@
 	id="contact"
 	class="relative z-10 rounded-t bg-gradient-to-b from-slate-100 to-background py-16 pt-24 text-center md:py-24 md:pt-32 dark:from-slate-900"
 >
-	<h2 class="font-poppins mb-4 text-3xl font-bold tracking-tight">Get in Touch</h2>
+	<h2 class="font-poppins mb-4 text-3xl font-bold tracking-tight">{m.contact_title()}</h2>
 	<p class="mx-auto mb-8 max-w-xl text-lg leading-relaxed text-muted-foreground">
-		I'm always open to discussing new projects or collaboration opportunities. <br
+		{m.contact_subtitle_first_part()} <br
 			class="hidden sm:inline"
-		/> Feel free to check out my work and reach out.
+		/> {m.contact_subtitle_second_part()}
 	</p>
 	<a
 		href="https://github.com/mikeu-dev"
 		target="_blank"
 		class="inline-flex h-11 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
 	>
-		Visit my GitHub <ArrowRight class="ml-2 size-4" />
+		{m.contact_button()} <ArrowRight class="ml-2 size-4" />
 	</a>
 </section>
