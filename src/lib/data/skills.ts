a3dc0d99978af131f8e4fc44dac7279586171d7d
Mikeu Dev <rikiruswandi28@gmail.com>
2025-12-22 13:19:45 +0700

    chore: exclude src/lib/data from git tracking

--- src/lib/data/skills.ts
+++
@@ -1,4 +0,0 @@
-export const skills: Record<string, string[]> = {
-	en: ['Fullstack Web Development', 'SvelteKit & Laravel', 'GIS Integration'],
-	id: ['Pemrograman Web Fullstack', 'SvelteKit & Laravel', 'Integrasi GIS']
-};
