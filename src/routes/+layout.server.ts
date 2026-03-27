1a7b8fae2ddf5e49adf8a83f764772c561973f8e
Mikeu Dev <rikiruswandi28@gmail.com>
2026-03-27 10:18:33 +0700

    refactor: Move Vercel serverless function configuration from `vercel.json` to `+layout.server.ts`.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,13 @@
 import type { LayoutServerLoad } from './$types';
 import { SocialsService } from '$lib/server/services/socials.service';
 import { VisitorService } from '$lib/server/services/visitor.service';
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+	runtime: 'nodejs22.x',
+	memory: 1024,
+	regions: ['sin1']
+};
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const socialsService = new SocialsService();
