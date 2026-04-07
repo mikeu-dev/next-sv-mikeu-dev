6041d0b44e00d6fbc558e9ca1978af821ccf00c1
Mikeu Dev <rikiruswandi28@gmail.com>
2026-04-07 18:02:53 +0700

    feat: add PWA support with manifest, icons, and service worker registration

---
+++ src/service-worker.ts
@@ -0,0 +1,75 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
+import { build, files, version } from '$service-worker';
+
+const self = globalThis as unknown as ServiceWorkerGlobalScope;
+
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build, // the app itself
+	...files // everything in `static`
+];
+
+self.addEventListener('install', (event: ExtendableEvent) => {
+	// Create a new cache and add all files to it
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event: ExtendableEvent) => {
+	// Delete old caches
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event: FetchEvent) => {
+	// ignore POST requests etc
+	if (event.request.method !== 'GET') return;
+
+	async function respond() {
+		const url = new URL(event.request.url);
+		const cache = await caches.open(CACHE);
+
+		// `build`/`files` can always be served from the cache
+		if (ASSETS.includes(url.pathname)) {
+			const response = await cache.match(url.pathname);
+			if (response) return response;
+		}
+
+		// for everything else, try the network first, but fall back to the cache if we're offline
+		try {
+			const response = await fetch(event.request);
+
+			// if we're offline, fetch can return a value that looks like a response but has a 404 status
+			if (!(response instanceof Response)) {
+				throw new Error('invalid response from fetch');
+			}
+
+			if (response.status === 200) {
+				cache.put(event.request, response.clone());
+			}
+
+			return response;
+		} catch {
+			const response = await cache.match(event.request);
+			if (response) return response;
+
+			// if there's no cache-hit, we're offline and have no cached version, return a generic error or a custom offline page
+			return new Response('Offline - cache miss', { status: 408 });
+		}
+	}
+
+	event.respondWith(respond());
+});
